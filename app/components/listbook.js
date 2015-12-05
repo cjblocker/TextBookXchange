15edad1c9c77b34585223db8f85d8e3639513488
Brian Watson <brianlwatson@gmail.com>
2015-12-05 12:05:07 -0700

    Adding AddBook capability to LIST

--- app/components/listbook.js
+++ app/components/listbook.js
@@ -1,17 +1,40 @@
 var React = require("react");
 var ReactRouter = require("react-router");
-
+var api = require("./api.js");
 var Link = ReactRouter.Link;
 
 
 var ListBook = React.createClass({
-      render: function() {
+
+    addBook: function(event) {
+        event.preventDefault();
+
+        var title = this.refs.booktitle;
+        var courseNumber = this.refs.coursenumber;
+        var edition = this.refs.edition;
+        var list_type = this.refs.listtype;
+        var price = this.refs.bookprice;
+
+        if(!title || !courseNumber || !edition || !list_type || !price) {
+            return;
+        }
+
+        api.addBook(title, courseNumber, edition, list_type, price, this.props.reload);
+        this.refs.title.value = '';
+        this.refs.courseNumber.value = '';
+        this.refs.edition.value = '';
+        this.refs.list_type.value = '';
+        this.refs.price.value = '';
+    },
+
+    render: function() {
         return (
             <div className='content'>
-      <div className='center' style={{width:300}}>
-            <p>
-                <h2> List a Textbook: </h2>
-                <form className="form-vertical" onSubmit={this.request}>
+                <div className='center' style={{width:300}}>
+                 <h2> List a Textbook: </h2>
+            
+               
+                <form className="form-vertical" onSubmit={this.addBook}>
                 <input type="text" placeholder="Book Title" ref="booktitle" autoFocus={true} />
                 <br/><br/>
                 <input type="text" placeholder="Course Number" ref="coursenumber"/>
@@ -25,12 +48,11 @@ var ListBook = React.createClass({
                 <input className="btn btn-warning" type="submit" value="Submit" />
                 </form>
 
-            </p>
-            </div>
+                </div>
             </div>
 
             );
-    }
+}
 
 
 });
