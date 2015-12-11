647819e4977a703f7d8473e2469d619baafbf6bd
Cameron Blocker <cameronjblocker@gmail.com>
2015-12-10 18:19:30 -0700

    Made requests that are available stand out with a dotted border

--- app/components/contact.js
+++ app/components/contact.js
@@ -16,12 +16,12 @@ var Contact = React.createClass({
          <div style={{display:"inline"}}>
           <div className="contact">
             <div className="circleImage" style={{backgroundImage: 'url(https://cdn.hackaday.io/images/resize/600x600/3360931415733128897.jpg)'}}></div>
-            <h3>Cameron Blocker</h3>
+            <h4>Cameron Blocker</h4>
             <p>cameronjblocker@gmail.com</p>
           </div>
           <div className="contact">
             <div className="circleImage" style={{backgroundImage: 'url('+brian_img+')'}}></div>
-                       <h3>Brian Watson</h3>
+                       <h4>Brian Watson</h4>
             <p>brianlwatson@gmail.com</p>
           </div>
          </div>
